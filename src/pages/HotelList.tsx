534d334acebc210e4f569f307f144675206d6b8c
H0onnn <dukei2017@naver.com>
2024-05-17 16:52:59 +0900

    Feat hotel list 구현

--- src/pages/HotelList.tsx
+++ src/pages/HotelList.tsx
@@ -1,3 +1,42 @@
+import React from "react";
+import { useHotels } from "@components/hotel-list/hooks/useHotels";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import Top from "@/components/shared/Top";
+import { Hotel } from "@/components/hotel-list/Hotel";
+import Spacing from "@/components/shared/Spacing";
+
 export const HotelList = () => {
-  return <div>Hotel List</div>;
+  const { data: hotels, hasNextPage, loadMore } = useHotels();
+
+  return (
+    <div>
+      <Top title="인기 호텔" subtitle="호텔부터 펜션까지 최저가" />
+
+      <InfiniteScroll
+        dataLength={hotels?.length ?? 0}
+        hasMore={hasNextPage}
+        loader={<></>}
+        next={loadMore}
+        scrollThreshold="100px"
+      >
+        <ul>
+          {hotels?.map((hotel, idx) => (
+            <React.Fragment key={hotel.id}>
+              <Hotel hotel={hotel} />
+
+              {hotels.length - 1 !== idx && (
+                <Spacing
+                  size={8}
+                  backgroundColor="gray100"
+                  style={{ margin: "20px 0" }}
+                />
+              )}
+            </React.Fragment>
+          ))}
+        </ul>
+      </InfiniteScroll>
+    </div>
+  );
 };
