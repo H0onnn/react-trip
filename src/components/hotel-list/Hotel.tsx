ef76634136d78ec9b40ba9def1c606ff9660e451
H0onnn <dukei2017@naver.com>
2024-05-20 15:43:42 +0900

    Feat Tag 컴포넌트 & 타이머 적용

--- src/components/hotel-list/Hotel.tsx
+++ src/components/hotel-list/Hotel.tsx
@@ -1,5 +1,9 @@
 /** @jsxImportSource @emotion/react */
 
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import dayjs from "dayjs";
 import { css } from "@emotion/react";
 
 import { Hotel as HotelProps } from "@models/hotel";
@@ -8,30 +12,86 @@ import Flex from "@shared/Flex";
 import Spacing from "@shared/Spacing";
 import Text from "@shared/Text";
 import addDelimiter from "@utils/addDelimiter";
+import Tag from "@shared/Tag";
+
+import formatTime from "@utils/formatTime";
 
 export const Hotel = ({ hotel }: { hotel: HotelProps }) => {
+  const [remainedTime, setReaminedTime] = useState(0);
+
+  useEffect(() => {
+    if (hotel.events == null || hotel.events.promoEndTime == null) {
+      return;
+    }
+
+    const { promoEndTime } = hotel.events;
+
+    const endTime = dayjs(promoEndTime).valueOf();
+
+    const timer = setInterval(() => {
+      const remainedSecond = endTime - dayjs().valueOf();
+
+      if (remainedSecond <= 0) {
+        clearInterval(timer);
+        return;
+      }
+
+      setReaminedTime(remainedSecond);
+    }, 1_000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [hotel.events]);
+
+  const tagComponent = () => {
+    if (hotel.events == null) {
+      return null;
+    }
+
+    const { name, tagThemeStyle } = hotel.events;
+
+    const promotionText =
+      remainedTime > 0 ? ` - ${formatTime(remainedTime)} 남음` : "";
+
+    return (
+      <div>
+        <Tag
+          color={tagThemeStyle.fontColor}
+          backgroundColor={tagThemeStyle.backgroundColor}
+        >
+          {name.concat(promotionText)}
+        </Tag>
+        <Spacing size={8} />
+      </div>
+    );
+  };
+
   return (
-    <ListRow
-      contents={
-        <Flex direction="column">
-          <ListRow.Texts title={hotel.name} subtitle={hotel.comment} />
+    <Link to={`/hotel/${hotel.id}`}>
+      <ListRow
+        contents={
+          <Flex direction="column">
+            {tagComponent()}
+            <ListRow.Texts title={hotel.name} subtitle={hotel.comment} />
 
-          <Spacing size={4} />
+            <Spacing size={4} />
 
-          <Text typo="t7" color="gray600">
-            {hotel.starRating}성급
-          </Text>
-        </Flex>
-      }
-      right={
-        <Flex direction="column" align="flex-end">
-          <img src={hotel.mainImageUrl} alt="hotel" css={imageStyles} />
-          <Spacing size={8} />
-          <Text bold={true}>{addDelimiter(hotel.price)}원</Text>
-        </Flex>
-      }
-      style={containerStyles}
-    />
+            <Text typo="t7" color="gray600">
+              {hotel.starRating}성급
+            </Text>
+          </Flex>
+        }
+        right={
+          <Flex direction="column" align="flex-end">
+            <img src={hotel.mainImageUrl} alt="hotel" css={imageStyles} />
+            <Spacing size={8} />
+            <Text bold={true}>{addDelimiter(hotel.price)}원</Text>
+          </Flex>
+        }
+        style={containerStyles}
+      />
+    </Link>
   );
 };
 
